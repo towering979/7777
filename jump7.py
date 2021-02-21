ec9868fc87cc57d8164b8689cf10bb0fe5d95a71
towering979 <1146633125zqw@qq.com>
2021-02-21 21:15:52 +0800

    22

--- jump7.py
+++ jump7.py
@@ -1,7 +1,14 @@
-a=0
-while a < 100:	
-	a+=1
+#a=0
+#while a < 100:	
+#	a+=1
+#	if a%7==0 or a%10==7 or a//10==7:
+#		continue
+#	else:
+#		print(a)
+
+for a in range(0,101):
 	if a%7==0 or a%10==7 or a//10==7:
 		continue
 	else:
 		print(a)
+
